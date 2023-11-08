9cab83e509249381960c147c9db1066cff5dae1a
tarunsinghal733 <20BCS3552cuchd.in>
2023-11-08 21:55:19 +0530

    vbgnhmjg

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -22,13 +22,13 @@ setData((prv) => [...prv,doc.data()])
     }, [])
 
     return (
-        <div className='flex flex-wrap justify-between p-3 mt-3'>
+        <div className='flex flex-wrap justify-between p-2 mt-2'>
             {Loading ? <div className='w-full flex items-center justify-center min-h-screen h-96'><InfinitySpin
                 height={40} color="white" /></div> :
                 Data.map((e, index) => {
                     return (
-                        <div key={index} className='Cardbg text-lg font-medium p-2 rounded-md hover:-translate-y-3 cursor-pointer transition-all ease-in-out 1s mt-6'>
-                            <img className='h-72 w-64 rounded' src={e.image}></img>
+                        <div key={index} className='md: w-40 h-96 Cardbg text-lg font-medium p-2 rounded-md hover:-translate-y-3 cursor-pointer transition-all ease-in-out 1s mt-6'>
+                            <img className='h-60 w-64 md:h-72 rounded' src={e.image}></img>
                             <h1><span className='card-heading'>Name: </span>{e.title}</h1>
                             <h1 className='flex items-center'><span className='card-heading pr-1'>Rating: </span><ReactStars count={5} size={24} value={5} half={true} edit={false}
                                 color2={'#ffd700'} /></h1>
